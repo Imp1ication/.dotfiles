fa9fe3238cea01d1a6cbba9086ce35bd80336d7c
Imp1ication <1mplicationxwzi@gmail.com>
2023-09-17 18:35:13 +0800

    first commit.

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,22 @@
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '$'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    set -l git_branch
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+	set git_branch " $git_branch"
+    end
+
+    echo -n -s (set_color $color_cwd) '' ' ' (prompt_pwd) (set_color yellow) ' ' $git_branch $normal $suffix " "
+end

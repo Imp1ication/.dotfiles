9ecaece83c40f4c526143ed8968cc70682980121
Imp1ication <1mplicationxwzi@gmail.com>
2023-09-21 17:17:02 +0800

    add lvim config

---
+++ lvim/config.lua
@@ -0,0 +1,19 @@
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+reload("user.options")
+reload("user.keymaps")
+reload("user.plugins")
+reload("user.copilot")
+reload("user.context")
+
+reload("core.bufferline")
+reload("core.lualine")
+reload("core.comment")
+reload("core.nvimtree")
+reload("core.cmp")
+reload("core.lspconfig")
+reload("core.null-ls")
+reload("core.alpha")
---
+++ lvim/lua/user/plugins.lua
@@ -0,0 +1,28 @@
+lvim.plugins = {
+    -- Colorscheme
+    { "sainnhe/sonokai" },
+
+    -- Rainbow parentheses
+    { "mrjones2014/nvim-ts-rainbow" },
+
+    -- Context
+    {"nvim-treesitter/nvim-treesitter-context"},
+
+    -- Copilot
+    { "zbirenbaum/copilot.lua" },
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
+}
+
+-- Colorscheme
+vim.o.background = "dark"
+
+vim.g.sonokai_style = "shusia"
+lvim.colorscheme = "tokyonight-night"
+
+-- Rainbow parentheses
+lvim.builtin.treesitter.rainbow.enable = true

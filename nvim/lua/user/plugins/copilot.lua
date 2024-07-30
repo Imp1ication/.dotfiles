aca7d6427b70ebdd4689d1f463d20022552de9cd
Imp1ication <1mplicationxwzi@gmail.com>
2024-07-30 08:57:21 +0800

    Rewrite copilot.lua

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
 	"zbirenbaum/copilot.lua",
-    enabled = false,
+	enabled = true,
 	event = "InsertEnter",
 	dependencies = {
 		"zbirenbaum/copilot-cmp",
@@ -9,28 +9,27 @@ return {
 		require("copilot").setup({
 			panel = { enabled = false },
 			suggestion = {
-				enabled = false,
+				enabled = true,
 				auto_trigger = true,
 				debounce = 75,
 				keymap = {
-					accept = "<M-a>",
-					accept_word = "<M-w>",
-					accept_line = "<M-l>",
-					next = "<M-j>",
-					prev = "<M-k>",
-					dismiss = "<M-y>",
+					accept = "<leader>ka",
+					accept_word = "<leader>kw",
+					accept_line = "<leader>kl",
+					next = "<leader>kj",
+					prev = "<leader>kk",
+					dismiss = "<leader>kd",
 				},
 			},
 			filetypes = {
-				yaml = false,
+                gitcommit = true,
 				markdown = true,
+				yaml = false,
 				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
 				cvs = false,
 				["."] = false,
+				["*.ini"] = false,
+				["*.config"] = false,
 			},
 			copilot_node_command = "node", -- Node.js version must be > 16.x
 			server_opts_overrides = {},

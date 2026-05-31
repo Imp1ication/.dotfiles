74feeeb9aaa21a9e21c7335f0691662e4744a74d
Imp1ication <1mplicationxwzi@gmail.com>
2026-05-31 16:15:28 +0800

    feat：delete vim-doge and disable autopairs.lua

--- nvim/lua/user/plugins/autopairs.lua
+++ nvim/lua/user/plugins/autopairs.lua
@@ -2,6 +2,7 @@ return {
 	"windwp/nvim-autopairs",
 	event = { "InsertEnter" },
 	dependencies = { "hrsh7th/nvim-cmp" },
+	enabled = false,
 	config = function()
 		require("nvim-autopairs").setup({
 			check_ts = true,
